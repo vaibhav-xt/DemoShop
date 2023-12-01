7cf7d2ad84524689e944f52f19477c539d5c58ae
Vaibhav Maurya <vibhu.astute@gmail.com>
2023-12-01 23:50:59 +0530

    modeified: updated base.scss & chages to search

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,11 +1,13 @@
 import SearchBar from "../components/SearchBar";
 import logo from '../assets/logo.webp'
+import LatestTrends from "../components/LatestTrends";
 
 export default function Search() {
     return (
         <div className="search-main-container">
             <img src={logo} alt="logo" className="logo" />
             <SearchBar />
+            <LatestTrends />
         </div>
     )
 }
