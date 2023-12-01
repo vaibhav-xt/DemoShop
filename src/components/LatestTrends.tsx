1e2f5a417def70f0b3de07bde6e01085af55420e
Vaibhav Maurya <vibhu.astute@gmail.com>
2023-12-02 00:19:25 +0530

    feat: added latest trends components

---
+++ src/components/LatestTrends.tsx
@@ -0,0 +1,41 @@
+
+export default function LatestTrends() {
+    return (
+        <div className='latestTrends-container'>
+            <p>Latest Trends</p>
+            <div>
+                {/* Trend Cards  */}
+                <div>
+                    <img src="https://i2.wp.com/minc-ecofashion.com/wp-content/uploads/2020/01/MINC-Puff-Sleeve-Formal-Shirt-In-Pinstripe-Black-Cotton.jpg?fit=1400%2C2000&ssl=1" alt="" />
+                    <p>Shirt with puffed sleeves</p>
+                </div>
+                <div>
+                    <img src="https://static3.azafashions.com/uploads/product_gallery/1686839651686_KK-337.jpg" alt="" />
+                    <p>Linem Jumpsuit</p>
+                </div>
+                <div>
+                    <img src="https://i2.wp.com/minc-ecofashion.com/wp-content/uploads/2020/01/MINC-Puff-Sleeve-Formal-Shirt-In-Pinstripe-Black-Cotton.jpg?fit=1400%2C2000&ssl=1" alt="" />
+                    <p>Shirt with puffed sleeves</p>
+                </div>
+                <div>
+                    <img src="https://i2.wp.com/minc-ecofashion.com/wp-content/uploads/2020/01/MINC-Puff-Sleeve-Formal-Shirt-In-Pinstripe-Black-Cotton.jpg?fit=1400%2C2000&ssl=1" alt="" />
+                    <p>Shirt with puffed sleeves</p>
+                </div>
+                <div>
+                    <img src="https://i2.wp.com/minc-ecofashion.com/wp-content/uploads/2020/01/MINC-Puff-Sleeve-Formal-Shirt-In-Pinstripe-Black-Cotton.jpg?fit=1400%2C2000&ssl=1" alt="" />
+                    <p>Shirt with puffed sleeves</p>
+                </div>
+            </div>
+
+            {/* Suggestions  */}
+            <p>Popular Suggestions</p>
+            <ul>
+                <li>Striped shirt dress</li>
+                <li>Satin shirts</li>
+                <li>Denim Jumpsuit</li>
+                <li>Leather Dresses</li>
+                <li>Solid tShirts</li>
+            </ul>
+        </div>
+    )
+}
