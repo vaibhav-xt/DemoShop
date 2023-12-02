6138844fcad08e0ced2899cf57b3e63b15648555
Vaibhav Maurya <vibhu.astute@gmail.com>
2023-12-02 11:16:39 +0530

    feat: added data to leatestTrends

--- src/components/LatestTrends.tsx
+++ src/components/LatestTrends.tsx
@@ -1,3 +1,4 @@
+import { data } from "../data"
 
 export default function LatestTrends() {
     return (
@@ -5,26 +6,14 @@ export default function LatestTrends() {
             <p>Latest Trends</p>
             <div>
                 {/* Trend Cards  */}
-                <div>
-                    <img src="https://i2.wp.com/minc-ecofashion.com/wp-content/uploads/2020/01/MINC-Puff-Sleeve-Formal-Shirt-In-Pinstripe-Black-Cotton.jpg?fit=1400%2C2000&ssl=1" alt="" />
-                    <p>Shirt with puffed sleeves</p>
-                </div>
-                <div>
-                    <img src="https://static3.azafashions.com/uploads/product_gallery/1686839651686_KK-337.jpg" alt="" />
-                    <p>Linem Jumpsuit</p>
-                </div>
-                <div>
-                    <img src="https://i2.wp.com/minc-ecofashion.com/wp-content/uploads/2020/01/MINC-Puff-Sleeve-Formal-Shirt-In-Pinstripe-Black-Cotton.jpg?fit=1400%2C2000&ssl=1" alt="" />
-                    <p>Shirt with puffed sleeves</p>
-                </div>
-                <div>
-                    <img src="https://i2.wp.com/minc-ecofashion.com/wp-content/uploads/2020/01/MINC-Puff-Sleeve-Formal-Shirt-In-Pinstripe-Black-Cotton.jpg?fit=1400%2C2000&ssl=1" alt="" />
-                    <p>Shirt with puffed sleeves</p>
-                </div>
-                <div>
-                    <img src="https://i2.wp.com/minc-ecofashion.com/wp-content/uploads/2020/01/MINC-Puff-Sleeve-Formal-Shirt-In-Pinstripe-Black-Cotton.jpg?fit=1400%2C2000&ssl=1" alt="" />
-                    <p>Shirt with puffed sleeves</p>
-                </div>
+                {
+                    data.slice(0, 5).map((product) => (
+                        <div>
+                            <img src={product.thumbnail} alt="img" />
+                            <p>{product.title}</p>
+                        </div>
+                    ))
+                }
             </div>
 
             {/* Suggestions  */}
