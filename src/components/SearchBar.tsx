f11b2c1f94dda1370d3f214ab417eaff810c9017
Vaibhav Maurya <vibhu.astute@gmail.com>
2023-12-02 12:29:03 +0530

    added logo to SearchBar.tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react"
+import logo from '../assets/logo.webp'
 
 export default function SearchBar() {
-
     const [search, setSearch] = useState<string>("")
 
     return (
-        <div className="search-container">
-            <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
-            <i className="fa-solid fa-magnifying-glass"></i>
-        </div>
+        <>
+            <img src={logo} alt="logo" className="logo" />
+            <div className="search-container">
+                <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+                <i className="fa-solid fa-magnifying-glass"></i>
+            </div>
+        </>
     )
 }
